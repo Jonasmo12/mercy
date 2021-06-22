a12a405fe92570ea352b69a2e2e094662ff33a53
Jonas Moloto <jonasmoloto@outlook.com>
2021-06-22 23:41:58 +0200

    refactored email config

--- learn/settings/production.py
+++ learn/settings/production.py
@@ -119,12 +119,12 @@ USE_TZ = True
 # email stuff
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = env('EMAIL_HOST')
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = env('EMAIL_HOST_USER')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 STATICFILES_DIRS = BASE_DIR / 'static',
